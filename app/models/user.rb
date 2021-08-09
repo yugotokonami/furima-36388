df7f1427f2c1305abfbe9b494f413ac35be473b4
yugotokonami <18b1123@andrew.ac.jp>
2021-08-09 15:18:39 +0900

    ユーザー管理機能の編集（単体テストコード以外）

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
+end
