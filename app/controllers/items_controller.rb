63de09c8828f5d8fdc3dc206f0be0e365ea9b6f2
yugotokonami <18b1123@andrew.ac.jp>
2021-08-11 10:37:58 +0900

    商品出品機能の編集（state,postage,shipping_dateモデルの作成とitemモデルの再編集）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,10 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,15 +4,21 @@ class Item < ApplicationRecord
     validates :name
     validates :catch_copy
     validates :price
-    validates :state_id
-    validates :postage_id
-    validates :prefecture_id
-    validates :shipping_date_id
-  end
 
     validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :state_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+  end
 
-    extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :prefecture
+    belongs_to :state
+    belongs_to :postage
+    belongs_to :shipping_date
+
+    belongs_to :user
+    has_one_attached :image
 end
