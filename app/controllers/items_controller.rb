7df37d67452d7722663bf5a6e68bad102d136023
yugotokonami <18b1123@andrew.ac.jp>
2021-08-12 18:32:27 +0900

    商品詳細機能の編集（実装段階まで）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.order("created_at DESC")
@@ -16,6 +16,21 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
